fc1aa70368a6dc459ebad210e7aabd5380c8f002
fullstackmiguel <miguelalexandredev@gmail.com>
2021-11-18 20:09:21 -0300

    Desbugando React

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Foda-se Todo mundo
+          Learn React
         </a>
       </header>
     </div>
